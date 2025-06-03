8dcf8b3336df8ec6cd9b684bdd26093ac9f4cd54
Sergey Chechenkin <schechenkin@ozon.ru>
2025-06-03 12:39:00 +0300

    02_runtime_init

---
+++ src/bsp/mod.rs
@@ -0,0 +1,4 @@
+#[path = "raspberrypi/cpu.rs"]
+mod raspberrypi;
+
+pub use raspberrypi::*;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 #![no_std]
 #![no_main]
 
+mod boot;
+mod bsp;
+mod cpu;
 use core::panic::PanicInfo;
-mod arch_boot;
+
+/// Early init code.
+///
+/// # Safety
+///
+/// - Only a single core must be active and running this function.
+unsafe fn kernel_init() -> ! {
+    panic!()
+}
 
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
-    loop {}
+    cpu::wait_forever()
 }
